d9a72559f3606019cadb42384c01993d1b4593b1
Evan Machnic <emachnic@gmail.com>
2020-04-06 08:41:07 -0400

    Updates

---
+++ precache-manifest.63d6dd2d68d5cced4ea71f27bfb08aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdca72357330282bdb2f4e5d14f3591b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4409a75519f87fc737f",
+    "url": "/monsters-rolodex/static/css/main.7cdb588b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4409a75519f87fc737f",
+    "url": "/monsters-rolodex/static/js/main.dc8e83fe.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
